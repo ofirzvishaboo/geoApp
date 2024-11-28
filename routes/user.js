d068f8e82b549d4e32a48b10e06a26600bcb8de3
ofirzvishaboo <ofirfirst@gmail.com>
2024-11-28 17:33:30 +0200

    Adding login, logout and register. adding toReturnTo in middleware

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
+const { storeReturnTo } = require('../middleware');
+
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
@@ -27,11 +29,19 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
     res.redirect(redirectUrl);
 });
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
 module.exports = router;
