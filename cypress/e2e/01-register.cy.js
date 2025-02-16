00b5170bc4b208f661eba5f0807dd8fc0a4f6f5a
ofirzvishaboo <ofirfirst@gmail.com>
2025-02-17 01:20:52 +0200

    changing hname to hardcoded

--- cypress/e2e/01-register.cy.js
+++ cypress/e2e/01-register.cy.js
@@ -2,8 +2,8 @@ require('dotenv').config();
   describe('template spec', () => {
     it('checking register', () => {
       cy.visit('/register');
-      cy.get('input[name=username]').type(Cypress.env('DB_USERNAME'));
-      cy.get('input[name=username]').should('have.value', Cypress.env('DB_USERNAME'));
+      cy.get('input[name=username]').type(Cypress.env('yakov'));
+      // cy.get('input[name=username]').should('have.value', Cypress.env('DB_USERNAME'));
       cy.get('input[name=email]').type('wow@examle.com');
       cy.get('input[name=email]').should('have.value', 'wow@examle.com');
       cy.get('input[name=password]').type(Cypress.env('DB_PASSWORD'));
