c603e622d468f1329d5719d7dd3df93164e0ffa0
ofirzvishaboo <ofirfirst@gmail.com>
2025-02-17 00:57:16 +0200

    seeds fix

--- seeds/index.js
+++ seeds/index.js
@@ -5,8 +5,8 @@ const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 // const User = require('../models/user');
 
-mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`);
-
+mongoose.connect('mongodb://localhost:27017/yelpCamp');
+// `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
@@ -19,19 +19,19 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    // await User.deleteMany({});
+    await User.deleteMany({});
 
-    // const user = new User({
-    //     email: process.env.ADMIN_MAIL,
-    //     username: process.env.ADMIN_USER
-    // });
-    // const registeredUser = await User.register(user, process.env.ADMIN_PASS);
+    const user = new User({
+        email: process.env.ADMIN_MAIL,
+        username: process.env.ADMIN_USER
+    });
+    const registeredUser = await User.register(user, process.env.ADMIN_PASS);
 
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '6750c26db59f5add7f1388e3', // registeredUser._id
+            author: registeredUser._id, // registeredUser._id
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2VzfGVufDB8fDB8fHww`,
