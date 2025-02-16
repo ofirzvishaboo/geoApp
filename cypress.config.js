68b35a873226daed89fb5471d8aab7f38707626c
ofirzvishaboo <ofirfirst@gmail.com>
2025-02-17 01:17:47 +0200

    changing secret

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 const dotenv = require('dotenv');
 dotenv.config();
-
+// 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
